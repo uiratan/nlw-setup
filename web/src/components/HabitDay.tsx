57d445cdf7576f898a07ef3ebed346297bf71b02
uiratan <uiratan@gmail.com>
2023-01-19 18:40:41 -0300

    Projeto completo aula 3

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,16 @@
+// interface HabitProps {
+//     completed: number
+// }
+
+// export function Habit(props: HabitProps) {
+//     return (
+//         //<div className="bg-zinc-900 w-10">{props.completed}</div>
+//         <div>{props.completed}</div>
+//     ) 
+// }
+
+export function HabitDay() {
+    return (
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>        
+    ) 
+}
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,52 @@
+import { generateDaysFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+
+const weekDays = [
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S',
+]
+
+const summaryDates = generateDaysFromYearBeginning()
+
+const minimumSummaryDaysSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDaysSize - summaryDates.length
+
+export function SymmaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center" >
+              {weekDay}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map(date => {
+          return <HabitDay key={date.toString()} />
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+            <div
+              key={i}
+              className="w-10 h-10 bg-zinc-300 opacity-40 cursor-not-allowed border-2 border-zinc-800 rounded-lg"
+            />
+          )
+
+
+        })}
+      </div>
+    </div >
+  )
+}
