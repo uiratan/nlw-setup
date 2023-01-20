8ada2a54b3fbeeabcb770f7c64dc3ce8b353d655
uiratan <uiratan@gmail.com>
2023-01-20 18:20:29 -0300

    aula 3 - server completo - web completo - mobile metade

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -33,15 +33,23 @@ export function SymmaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          )
         })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
             <div
               key={i}
-              className="w-10 h-10 bg-zinc-300 opacity-40 cursor-not-allowed border-2 border-zinc-800 rounded-lg"
-            />
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg flex items-center justify-center"
+            >
+              X
+            </div>
           )
 
 
